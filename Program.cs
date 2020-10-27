cd88803afe13051894d4439632fc1bfae4f5050c
Lacey Walker <bluelarksong@gmail.com>
2020-10-27 13:33:40 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace OceanNight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // transform a bunch of text into a list of words
+
+            // read text from the file
+
+            // print the list of words and counts
+        }
+    }
+}
